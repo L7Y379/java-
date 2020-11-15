2cfa0510b15e4df216958b80802b31763da4445d
L7Y379 <1102907282@qq.com>
2020-11-15 22:55:57 +0800

    Update-MethodTest

---
+++ Test_jiedian/MethodTest/MethodTest04.java
@@ -0,0 +1,33 @@
+
+/*
+	方法的调用不一定在main方法中，可以在其他方法中，
+	只要是程序可以执行到的位置，都可以调用其他方法
+*/
+
+public class MethodTest04{
+	
+	public static void sum(int a,int b){
+		System.out.println(a+"+"+b+"="+(a+b));
+		
+		//调用doSome方法
+		MethodTest04.doSome();
+	}
+	
+	//主方法
+	public static void main(String[] args){
+		
+		//调用sum方法
+		MethodTest04.sum(1,2);
+		
+		System.out.println("Hello World!");
+		System.out.println("5*9="+mul(5,9));
+	}
+	
+	public static void doSome(){
+		System.out.println("do some!");
+	}
+	
+	public static int mul(int a,int b){
+		return (a*b);
+	}
+}
