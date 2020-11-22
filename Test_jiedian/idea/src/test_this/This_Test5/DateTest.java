34ac2efe66ebdbd978e85356a72b1be11f2e010c
L7Y379 <1102907282@qq.com>
2020-11-23 01:03:44 +0800

    Update_ThisTest

---
+++ Test_jiedian/idea/src/test_this/This_Test5/DateTest.java
@@ -0,0 +1,15 @@
+package test_this.This_Test5;
+
+public class DateTest {
+    public static void main(String[] args) {
+
+        //创建一个对象
+        Date time1=new Date();
+        time1.print();
+
+        //创建另一个对象
+        Date time2=new Date(2008,8,8);
+        time2.print();
+
+    }
+}
